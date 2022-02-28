eee2a707b6ee408b196ede1fd10ceca017783171
k3ix <egormishuk@gmail.com>
2022-02-28 17:50:58 +0300

    lang

--- models/Users.js
+++ models/Users.js
@@ -21,11 +21,7 @@ module.exports = (sequelize, DataTypes) => {
         theme: {
             type: DataTypes.STRING,
             allowNull: false,
-        },
-        lang: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
+        }
     });
 
     return Users;
